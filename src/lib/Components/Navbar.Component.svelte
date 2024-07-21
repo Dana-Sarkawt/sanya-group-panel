886840f76537f3185461e1440c17a5ec5bd4d40e
Dana-Sarkawt <dana.code01@gmail.com>
2024-07-21 16:54:11 +0300

    Update app.css and app.html, and Navbar.Component.svelte

--- src/lib/Components/Navbar.Component.svelte
+++ src/lib/Components/Navbar.Component.svelte
@@ -1,25 +1,27 @@
 <script>
       import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Dropdown, DropdownItem, DropdownDivider } from 'flowbite-svelte';
   import { ChevronDownOutline } from 'flowbite-svelte-icons';
+  import { DarkMode } from 'flowbite-svelte';
+  import { ThumbsUpSolid, ThumbsDownSolid } from 'flowbite-svelte-icons';
   import { page } from '$app/stores';
   $: activeUrl = $page.url.pathname;
   </script>
 
-<Navbar let:NavContainer >
-    <NavContainer class="border px-5 py-2 rounded-lg bg-[#24b97d] text-white">
+<Navbar let:NavContainer class="bg-transparent dark:bg-transparent">
+    <NavContainer class="border px-5 h-26 rounded-lg md:rounded-full border-[#24b97d] bg-[#24b97d7e] text-white">
       <NavBrand href="/">
         <!-- <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" /> -->
-        <span class="self-center whitespace-nowrap text-xl font-semibold text-white">Sanya Group</span>
+        <span class="self-center whitespace-nowrap text-xl font-semibold text-[#104e35] dark:text-white">Sanya Group</span>
       </NavBrand>
-      <NavHamburger/>
-      <NavUl nonActiveClass="text-white" ulClass="bg-[#198359] md:bg-transparent border-0 mt-4  flex flex-col p-4 mt-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:text-sm md:font-medium">
+      <NavHamburger menuClass="text-[#104e35] dark:text-white"/>
+      <NavUl nonActiveClass="text-[#104e35] dark:text-white" ulClass="bg-transparent dark:bg-transparent md:bg-transparent dark:md:bg-transparent border-0 mt-4  flex items-center flex-col p-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:text-sm md:font-medium">
         <NavLi class="cursor-pointer">
-            Company<ChevronDownOutline class="w-6 h-6 ms-2 text-white dark:text-white inline" />
+            Company<ChevronDownOutline class="w-6 h-6 ms-2 text-[#104e35] dark:text-white inline" />
           </NavLi>
           <Dropdown class="w-44 z-20">
             <DropdownItem href="/">Project</DropdownItem>
             <DropdownItem >
-                <NavLi class="cursor-pointer text-black">
+                <NavLi class="cursor-pointer text-black dark:text-white">
                     Expenses<ChevronDownOutline class="w-6 h-6 ms-2 text-green-500  inline" />
                   </NavLi>
 
@@ -34,9 +36,17 @@
             <DropdownItem href="/">Sales</DropdownItem>
           </Dropdown>
 
-        <NavLi href="/about">Users</NavLi>
+        <NavLi href="/user/1">Users</NavLi>
         <NavLi href="/docs/components/navbar">Setting</NavLi>
         <NavLi href="/pricing">Logout</NavLi>
+
+
+        <DarkMode class="text-lg" btnClass = 'text-gray-500 dark:text-gray-400  rounded-lg text-sm p-2.5'>
+            <img src="/images/sun.png" slot="lightIcon" class="w-32  object-cover" alt="">
+            <img src="/images/moon.png" slot="darkIcon" class="w-32 object-cover" alt="">
+        </DarkMode>
+
+
       </NavUl>
     </NavContainer>
   </Navbar>
---
+++ src/lib/Components/Tables/User.Table.Component.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import {
+		Table,
+		TableBody,
+		TableBodyCell,
+		TableBodyRow,
+		TableHead,
+		TableHeadCell
+	} from 'flowbite-svelte';
+	
+</script>
+
+
+    <Table striped={true}>
+        <TableHead theadClass="bg-white dark:bg-[#212121]">
+            <TableHeadCell>Apollo ID</TableHeadCell>
+            <TableHeadCell>Apollo Name</TableHeadCell>
+            <TableHeadCell>Owner Name</TableHeadCell>
+            <TableHeadCell>Physical Address</TableHeadCell>
+            <TableHeadCell>Status</TableHeadCell>
+            <TableHeadCell>Action</TableHeadCell>
+        </TableHead>
+        <TableBody tableBodyClass="divide-y">
+                    <TableBodyRow>
+                        <TableBodyCell>12</TableBodyCell>
+                        <TableBodyCell>df</TableBodyCell>
+                        <TableBodyCell>
+                            <div class="justfiy-center flex h-auto w-auto items-center gap-2">
+                                <img src="/images/photo.png" class="h-10 w-10 rounded-lg object-contain" alt="" />
+                                <p>hjghj</p>
+                            </div>
+                        </TableBodyCell>
+                        <TableBodyCell>yyuyu</TableBodyCell>
+                        <TableBodyCell
+                            tdClass="px-6 py-4 whitespace-nowrap font-medium  flex justify-center items-center"
+                        >
+                            <div
+                                class="justfiy-center flex h-8 w-28 items-center rounded-full  px-2"
+                            >
+                                <p
+                                    class="w-24"
+                                >
+                                    jkjkjk
+                                </p>
+                            </div>
+                        </TableBodyCell>
+    
+                        <TableBodyCell>
+                            <div class="flex h-auto w-auto items-center justify-center gap-2">
+                                
+                                    <a href="edit/1">
+                                        <img src="/images/edit.png" class="h-6 w-6 object-contain" alt="" />
+                                    </a>
+                                
+                            </div>
+                        </TableBodyCell>
+                    </TableBodyRow>
+        </TableBody>
+    </Table>

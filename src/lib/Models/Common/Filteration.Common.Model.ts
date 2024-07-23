17d899e415ecf1b640159878183f31703e7bca0f
Dana-Sarkawt <dana.code01@gmail.com>
2024-07-23 11:30:34 +0300

    Add userStore import and Pagination component

---
+++ src/lib/Models/Common/Filteration.Common.Model.ts
@@ -0,0 +1,6 @@
+import { ListAttributesRequest } from '../Request/ListAttributes.Request.Model';
+
+export class Filtration<T> extends ListAttributesRequest {
+	options?: T;
+	field?: string;
+}
---
+++ src/lib/Models/Request/ListAttributes.Request.Model.ts
@@ -0,0 +1,6 @@
+export class ListAttributesRequest {
+	page?: number = 0;
+	limit?: number = 10;
+	sort?: string;
+	sortDescending?: boolean;
+}

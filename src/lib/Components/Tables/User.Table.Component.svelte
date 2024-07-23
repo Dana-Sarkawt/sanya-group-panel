c5a8de2bb31adff20f4d438298ff2d1a27ec8386
RovarQRoom <rovar.07000120@outlook.com>
2024-07-23 09:55:03 +0300

    chore: Refactor user page server code and store
    The commit refactors the user page server code and store. It removes the deprecated `+page.server.ts` file and updates the `load` function in the `[page]+page.server.ts` file to use the `userStore` to fetch users. Additionally, it updates the `UsersRepository` implementation to select the exact count of users. This change improves the performance and maintainability of the user page functionality.
    
    Note: This commit does not include any changes to the CSS or HTML files.

--- src/lib/Components/Tables/User.Table.Component.svelte
+++ src/lib/Components/Tables/User.Table.Component.svelte
@@ -1,90 +1,64 @@
 <script lang="ts">
-	import {
-		Table,
-		TableBody,
-		TableBodyCell,
-		TableBodyRow,
-		TableHead,
-		TableHeadCell
-	} from 'flowbite-svelte';
-	
+  import type { Store } from "$lib/Models/Response/Store.Response.Model";
+  import type { Database } from "$lib/Supabase/Types/database.types";
+  import {
+    Table,
+    TableBody,
+    TableBodyCell,
+    TableBodyRow,
+    TableHead,
+    TableHeadCell,
+  } from "flowbite-svelte";
+
+  export let users: Store<Database["public"]["Tables"]["Users"]["Row"]> = {
+    data: [],
+    count: 0,
+  };
 </script>
 
-
 <div class="w-full h-auto bg-[#ffffff] dark:bg-[#081c18] p-4 rounded-b-xl">
+  <Table striped={true}>
+    <TableHead theadClass="bg-white dark:bg-[#212121] text-center">
+      <TableHeadCell>Name</TableHeadCell>
+      <TableHeadCell>Phone</TableHeadCell>
+      <TableHeadCell>Email</TableHeadCell>
+      <TableHeadCell>Action</TableHeadCell>
+    </TableHead>
+    <TableBody tableBodyClass="divide-y text-center">
+      {#if users.count !== 0}
+        {#each users.data as user}
+          <TableBodyRow>
+            <TableBodyCell>{user.name}</TableBodyCell>
+            <TableBodyCell>{user.phone}</TableBodyCell>
+            <TableBodyCell>{user.email}</TableBodyCell>
 
-    <Table striped={true}>
-        <TableHead theadClass="bg-white dark:bg-[#212121] text-center">
-            <TableHeadCell>Name</TableHeadCell>
-            <TableHeadCell>Phone</TableHeadCell>
-            <TableHeadCell>Email</TableHeadCell>
-            <TableHeadCell>Action</TableHeadCell>
-        </TableHead>
-        <TableBody tableBodyClass="divide-y text-center">
-                    <TableBodyRow>
-                        <TableBodyCell>12</TableBodyCell>
-                        <TableBodyCell>df</TableBodyCell>
-                        <TableBodyCell>yyuyu</TableBodyCell>
-    
-                        <TableBodyCell>
-                            <div class="flex h-auto w-auto items-center justify-center gap-2">
-                                
-
-                                    <a href="edit/1" class="bg-green-600 hover:bg-green-500 h-12 w-12 p-2 flex justify-center items-center rounded-full">
-                                        <img src="/images/edit.png" class="h-8 w-8 object-contain" alt="" />
-                                    </a>
-                                    
-                                    <a href="edit/1" class="bg-red-600 hover:bg-red-500 h-12 w-12 p-2 flex justify-center items-center rounded-full">
-                                        <img src="/images/delete.png" class="h-8 w-8 object-contain" alt="" />
-                                    </a>
-                                
-                            </div>
-                        </TableBodyCell>
-                    </TableBodyRow>
-
-
-                    <TableBodyRow>
-                        <TableBodyCell>12</TableBodyCell>
-                        <TableBodyCell>df</TableBodyCell>
-                        <TableBodyCell>yyuyu</TableBodyCell>
-    
-                        <TableBodyCell>
-                            <div class="flex h-auto w-auto items-center justify-center gap-2">
-                                
-
-                                    <a href="edit/1" class="bg-green-600 hover:bg-green-500 h-12 w-12 p-2 flex justify-center items-center rounded-full">
-                                        <img src="/images/edit.png" class="h-8 w-8 object-contain" alt="" />
-                                    </a>
-                                    
-                                    <a href="edit/1" class="bg-red-600 hover:bg-red-500 h-12 w-12 p-2 flex justify-center items-center rounded-full">
-                                        <img src="/images/delete.png" class="h-8 w-8 object-contain" alt="" />
-                                    </a>
-                                
-                            </div>
-                        </TableBodyCell>
-                    </TableBodyRow>
-
-
-                    <TableBodyRow>
-                        <TableBodyCell>12</TableBodyCell>
-                        <TableBodyCell>df</TableBodyCell>
-                        <TableBodyCell>yyuyu</TableBodyCell>
-    
-                        <TableBodyCell>
-                            <div class="flex h-auto w-auto items-center justify-center gap-2">
-                                
-
-                                    <a href="edit/1" class="bg-green-600 hover:bg-green-500 h-12 w-12 p-2 flex justify-center items-center rounded-full">
-                                        <img src="/images/edit.png" class="h-8 w-8 object-contain" alt="" />
-                                    </a>
-                                    
-                                    <a href="edit/1" class="bg-red-600 hover:bg-red-500 h-12 w-12 p-2 flex justify-center items-center rounded-full">
-                                        <img src="/images/delete.png" class="h-8 w-8 object-contain" alt="" />
-                                    </a>
-                                
-                            </div>
-                        </TableBodyCell>
-                    </TableBodyRow>
-        </TableBody>
-    </Table>
+            <TableBodyCell>
+              <div class="flex h-auto w-auto items-center justify-center gap-2">
+                <a
+                  href="edit/1"
+                  class="bg-green-600 hover:bg-green-500 h-12 w-12 p-2 flex justify-center items-center rounded-full"
+                >
+                  <img
+                    src="/images/edit.png"
+                    class="h-8 w-8 object-contain"
+                    alt=""
+                  />
+                </a>
+                <a
+                  href="edit/1"
+                  class="bg-red-600 hover:bg-red-500 h-12 w-12 p-2 flex justify-center items-center rounded-full"
+                >
+                  <img
+                    src="/images/delete.png"
+                    class="h-8 w-8 object-contain"
+                    alt=""
+                  />
+                </a>
+              </div>
+            </TableBodyCell>
+          </TableBodyRow>
+        {/each}
+      {/if}
+    </TableBody>
+  </Table>
 </div>

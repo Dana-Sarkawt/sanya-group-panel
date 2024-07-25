b86106773ac8d19cb07cc3141959b0e1561964a9
Dana-Sarkawt <dana.code01@gmail.com>
2024-07-25 11:24:59 +0300

    Add user profile letter loading style and update page titles

--- src/routes/project/[projectId]/expense/preparation/add/+page.svelte
+++ src/routes/project/[projectId]/expense/preparation/add/+page.svelte
@@ -3,12 +3,24 @@
     import { User } from "$lib/Models/Request/User.Request.Model";
   import { dailyStore } from "$lib/Store/Daily.Store";
     import type { PageData } from "./$types";
+    import { page } from "$app/stores";
     export let data: PageData;
     // const capitalRequest = new Capital.Create();
   
   </script>
   
-  <p class="w-full h-auto text-4xl dark:text-white text-center my-12">Add Preparation</p>
+  <div class="w-full h-auto flex  justify-center items-center  md:px-44">
+
+    <a href="/project/{$page.params.projectId}/expense/0">
+    <p class="w-24 h-12 rounded-xl flex justify-center items-center bg-green-700 hover:bg-green-500 text-white duration-300 ease-in-out">Back</p>
+    </a>
+  
+    <p class="w-full h-auto text-2xl md:text-4xl dark:text-white text-center my-12">
+      Add Preparation
+    </p>
+  </div>
+
+
   <div class="w-full h-auto flex justify-center items-center">
     <div
       class="w-[90%] md:w-[50%] h-auto p-10 bg-[#94DCBA] dark:bg-[#11433A] border border-[#11433A] dark:border-[#94DCBA] rounded-xl flex flex-col justify-center items-center gap-6"

20b7049b25b4b4bda2861f2bc90bf3e25fb9be24
RovarQRoom <rovar.07000120@outlook.com>
2024-07-25 11:41:18 +0300

    chore: Refactor worker add page component

--- src/routes/project/[projectId]/expense/worker/add/+page.svelte
+++ src/routes/project/[projectId]/expense/worker/add/+page.svelte
@@ -1,12 +1,7 @@
 <script lang="ts">
-    import { goto } from "$app/navigation";
-    import { User } from "$lib/Models/Request/User.Request.Model";
-  import { dailyStore } from "$lib/Store/Daily.Store";
-    import type { PageData } from "./$types";
-    import { page } from "$app/stores";
-    export let data: PageData;
-    // const capitalRequest = new Capital.Create();
-	import { workerStore } from '$lib/Store/Worker.Store';
+  import { goto } from "$app/navigation";
+  import { page } from "$app/stores";
+  import { workerStore } from "$lib/Store/Worker.Store";
   import { Worker } from "$lib/Models/Request/Worker.Request.Model";
 
   const workerRequest = {
@@ -23,43 +18,40 @@
       console.log(error);
     }
   }
-  
-  </script>
-  
-  <div class="w-full h-auto flex  justify-center items-center  md:px-44">
+</script>
 
-    <a href="/project/{$page.params.projectId}/expense/0">
-    <p class="w-24 h-12 rounded-xl flex justify-center items-center bg-green-700 hover:bg-green-500 text-white duration-300 ease-in-out">Back</p>
-    </a>
-  
-    <p class="w-full h-auto text-2xl md:text-4xl dark:text-white text-center my-12">
-      Add Worker
-    </p>
-  </div>
-
-
-  <div class="w-full h-auto flex justify-center items-center">
-    <div
-      class="w-[90%] md:w-[50%] h-auto p-10 bg-[#94DCBA] dark:bg-[#11433A] border border-[#11433A] dark:border-[#94DCBA] rounded-xl flex flex-col justify-center items-center gap-6"
+<div class="w-full h-auto flex justify-center items-center md:px-44">
+  <a href="/project/{$page.params.projectId}/expense/0">
+    <p
+      class="w-24 h-12 rounded-xl flex justify-center items-center bg-green-700 hover:bg-green-500 text-white duration-300 ease-in-out"
     >
-    
-  
-      <div class="w-full h-auto flex flex-col justify-center items-start">
-        <p class="dark:text-white">Name</p>
-        <input
-          type="text"
-          class="w-full bg-[#daffee] dark:bg-[#0d2621] rounded-xl border-0 dark:text-white"
-          bind:value={workerRequest.name}
-        />
-      </div>
-  
- 
-  
-      <button
-        class="w-full h-12 rounded-xl bg-green-600 hover:bg-green-500 text-white duration-300 ease-in-out"
-        on:click={addWorker}
-        >Add Worker</button
-      >
+      Back
+    </p>
+  </a>
 
+  <p
+    class="w-full h-auto text-2xl md:text-4xl dark:text-white text-center my-12"
+  >
+    Add Worker
+  </p>
 </div>
+
+<div class="w-full h-auto flex justify-center items-center">
+  <div
+    class="w-[90%] md:w-[50%] h-auto p-10 bg-[#94DCBA] dark:bg-[#11433A] border border-[#11433A] dark:border-[#94DCBA] rounded-xl flex flex-col justify-center items-center gap-6"
+  >
+    <div class="w-full h-auto flex flex-col justify-center items-start">
+      <p class="dark:text-white">Name</p>
+      <input
+        type="text"
+        class="w-full bg-[#daffee] dark:bg-[#0d2621] rounded-xl border-0 dark:text-white"
+        bind:value={workerRequest.name}
+      />
+    </div>
+
+    <button
+      class="w-full h-12 rounded-xl bg-green-600 hover:bg-green-500 text-white duration-300 ease-in-out"
+      on:click={addWorker}>Add Worker</button
+    >
+  </div>
 </div>

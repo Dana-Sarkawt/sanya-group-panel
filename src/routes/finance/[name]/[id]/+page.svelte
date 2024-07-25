01a31d09272ba0e1631b29f51451f787b420a431
Dana-Sarkawt <dana.code01@gmail.com>
2024-07-25 10:29:11 +0300

    Remove unnecessary code and update page title

---
+++ src/routes/finance/[name]/[id]/+page.svelte
@@ -0,0 +1,118 @@
+<script lang="ts">
+    import DeleteModal from "$lib/Components/DeleteModal.Component.svelte";
+  import Pagination from "$lib/Components/Pagination.Component.svelte";
+    import type { Store } from "$lib/Models/Response/Store.Response.Model";
+    import type { Database } from "$lib/Supabase/Types/database.types";
+    import { page } from "$app/stores";
+  import type { PageData } from "./$types";
+  import ProjectTable from "$lib/Components/ResponsiveTable/ProjectTable.Component.svelte";
+  export let data: PageData;
+    export let capitals: Store<Database["public"]["Tables"]["Capitals"]["Row"]> =
+      {
+        data: [],
+        count: 0,
+      };
+    let deleteModal = false;
+  </script>
+  
+
+  <div class=" w-full h-auto flex justify-center items-center">
+    <div
+      class="  flex h-[100vh] w-full flex-col justify-start items-center lg:mx-24"
+      id="subDiv"
+    >
+      <div
+        class="flex h-24 w-full items-center justify-start pl-2 text-4xl text-[#0F4E35] dark:text-white font-bold"
+      >
+      Financial Table
+      </div>
+  
+      <div
+        class="flex h-16 w-full items-center justify-end rounded-t-lg p-2 dark:bg-[#081c18] bg-[#ffffff]"
+      >
+        <a href="/finance/*/*/add">
+          <button
+            class="h-12 rounded-lg bg-[#24b97d] px-4 text-white"
+            style="box-shadow:0 1px 8px 0px #24b97d;"
+            ><span>+</span> Add Financial</button
+          >
+        </a>
+      </div>
+
+
+
+
+
+
+<!---  START TABLE  -->
+
+  <div class="w-full h-auto flex justify-center items-center mx-2">
+    <table class="table w-full text-white text-[5px] md:text-lg rounded-xl">
+      <thead>
+        <tr>
+          <th scope="col">Description</th>
+          <th scope="col">Price</th>
+          <th scope="col">Date</th>
+          <th scope="col">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        {#if capitals.count !== 0}
+          {#each capitals.data as capital}
+            <tr>
+              <td>{capital.description}</td>
+              <td>{capital.price}</td>
+              <td>{capital.date}</td>
+  
+              <td>
+                <!-- svelte-ignore a11y-click-events-have-key-events -->
+                <!-- svelte-ignore a11y-no-static-element-interactions -->
+                <!-- svelte-ignore a11y-missing-attribute -->
+                <div class="flex h-auto w-auto items-center justify-center gap-2">
+                  <a
+                    href="edit/1"
+                    class="bg-green-600 hover:bg-green-500 w-6 h-6 md:h-12 md:w-12 p-2 flex justify-center items-center rounded-full"
+                  >
+                    <img
+                      src="/images/edit.png"
+                      class="w-4 h-4 md:h-8 md:w-8 object-contain"
+                      alt=""
+                    />
+                  </a>
+                  <!-- svelte-ignore a11y-click-events-have-key-events -->
+                  <a
+                    class="bg-red-600 hover:bg-red-500 w-6 h-6 md:h-12 md:w-12 p-2 flex justify-center items-center rounded-full"
+                    on:click={() => (deleteModal = true)}
+                  >
+                    <img
+                      src="/images/delete.png"
+                      class="w-4 h-4 md:h-8 md:w-8 object-contain"
+                      alt=""
+                    />
+                  </a>
+                </div>
+              </td>
+            </tr>
+          {/each}
+        {/if}
+      </tbody>
+    </table>
+  </div>
+
+  <!---  END TABLE  -->
+
+
+
+
+  <div class="w-full h-auto flex justify-center items-center py-12">
+    <!-- <Pagination
+      classType={typeof data.projects}
+      StoreData={data.projects}
+      currentPage={Number($page.params.page)}
+      name={"project"}
+    /> -->
+  </div>
+</div>
+</div>
+  <DeleteModal bind:deleteModal />
+  

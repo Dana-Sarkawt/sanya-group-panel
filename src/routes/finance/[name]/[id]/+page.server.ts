6c6ed844c3be8bfe9fd75480f85a5cd3f262fd6c
RovarQRoom <rovar.07000120@outlook.com>
2024-07-29 09:50:48 +0300

    refactor: Remove console.log statement in +page.server.ts

--- src/routes/finance/[name]/[id]/+page.server.ts
+++ src/routes/finance/[name]/[id]/+page.server.ts
@@ -2,8 +2,6 @@ import { financialDueStore } from "$lib/Store/FinancialDue.Store.js";
 
 export async function load({ params, url }) {
   const field = `${params.name}_id`;
-  console.log(params);
-  
   const financialDues = await financialDueStore.getAll({
     field: field,
     equal: params.id,

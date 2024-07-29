6412f642a397e7e8898e72f6a0dc701966f1a444
Dana-Sarkawt <dana.code01@gmail.com>
2024-07-29 10:09:03 +0300

    Add pagedone package and update Navbar.Component.svelte

--- src/lib/Components/Navbar.Component.svelte
+++ src/lib/Components/Navbar.Component.svelte
@@ -6,10 +6,14 @@
     NavUl,
     NavHamburger,
   } from "flowbite-svelte";
-
+  import { page } from '$app/stores';
   import { DarkMode } from "flowbite-svelte";
   import { Tooltip } from "flowbite-svelte";
   import { authStore } from "$lib/Store/Auth.Store";
+
+  $: activeUrl = $page.url.pathname;
+  let activeClass = 'text-green-500 bg-green-700 md:bg-transparent md:text-green-700 md:dark:text-green-300 dark:bg-green-600 md:dark:bg-transparent duration-300 ease-in-out';
+  let nonActiveClass = 'text-white hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 dark:text-white md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent duration-300 ease-in-out';
 
   export let isLoading;
   let navBarOpening: boolean | undefined = undefined;
@@ -36,13 +40,13 @@
     >
       <NavBrand href="/" class="">
         <span
-          class="self-center whitespace-nowrap text-sm lg:text-xl font-semibold text-[#104e35] dark:text-white mr-12"
+          class="self-center whitespace-nowrap text-sm lg:text-xl font-semibold text-[#104e35] dark:text-white mr-12 "
           >Sanya Group</span
         >
       </NavBrand>
 
       <div
-        class="w-12 h-12 rounded-full border border-[#24b97d] bg-[#24b97d7e] dark:bg-[#11433A] flex md:hidden my-4 justify-center items-center"
+        class="w-12 h-12 rounded-full border border-[#24b97d] bg-[#24b97d7e] dark:bg-[#11433A] flex md:hidden my-4 justify-center items-center mr-12"
       >
         {#if (isLoading && !$authStore) || $authStore?.name === undefined}
           <span class="loader"></span>
@@ -83,12 +87,18 @@
       />
 
       <NavUl
+      {activeUrl} {activeClass} {nonActiveClass}
         class="navbarContainer"
-        nonActiveClass="text-[#104e35] dark:text-white"
+        
         ulClass="bg-transparent dark:bg-transparent md:bg-transparent dark:md:bg-transparent border-0 mt-4  flex items-center flex-col p-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 text-xs lg:text-md xl:text-lg shrink-10"
       >
-        <NavLi href="/project/0" on:click={toggleNavBar}>Project</NavLi>
-        <NavLi href="/user/0" on:click={toggleNavBar}>Users</NavLi>
+        <NavLi href="/project/0" class= {activeUrl.startsWith("/project") ? activeClass : nonActiveClass}
+        on:click={toggleNavBar}>Project</NavLi>
+
+        <NavLi href="/user/0" class= {activeUrl.startsWith("/user") ? activeClass : nonActiveClass}
+        on:click={toggleNavBar}>Users</NavLi>
+
+
         <!-- <NavLi href="/setting">Setting</NavLi> -->
         <NavLi class="cursor-pointer" on:click={() => authStore.logout()}
           >Logout</NavLi
@@ -96,19 +106,19 @@
 
         <DarkMode
           class="text-lg"
-          btnClass="text-gray-500 dark:text-gray-400  rounded-lg text-sm p-2.5"
+          btnClass="text-gray-500 dark:text-gray-400  rounded-lg text-sm p-2.5 "
         >
           <img
             src="/images/sun.png"
             slot="lightIcon"
-            class="w-8 object-cover"
+            class="w-4 md:w-8 object-cover"
             alt=""
           />
           
           <img
             src="/images/moon.png"
             slot="darkIcon"
-            class="w-8 object-cover"
+            class="w-4 md:w-8 object-cover"
             alt=""
           />
         </DarkMode>

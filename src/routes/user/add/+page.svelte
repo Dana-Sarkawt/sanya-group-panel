1736d2e1827b787a7e2f16337fdd3cc21d523628
RovarQRoom <rovar.07000120@outlook.com>
2024-07-30 11:18:06 +0300

    chore: Remove commented code and improve phone input validation in +page.svelte

--- src/routes/user/add/+page.svelte
+++ src/routes/user/add/+page.svelte
@@ -5,6 +5,7 @@
   import type { PageData } from "./$types";
   export let data: PageData;
   const userRequest = new User.Create();
+  const regexPhone = /^07\d{9}$/;
 
   async function addUser(request: User.Create) {
     try {
@@ -17,23 +18,37 @@
       console.log(error);
     }
   }
+  function phoneField(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    const currentValue = inputElement.value;
+
+    // Check the length of the input value
+    if (currentValue.length > 11) {
+      inputElement.value = currentValue.slice(0, 11);
+      return;
+    }
+  }
 </script>
 
-<div class="w-full h-auto flex  justify-center items-center  md:px-44">
-
+<div class="w-full h-auto flex justify-center items-center md:px-44">
   <a href="/user/0">
-  <p class="w-24 h-12 rounded-xl flex justify-center items-center bg-green-700 hover:bg-green-500 text-white duration-300 ease-in-out">Back</p>
+    <p
+      class="w-24 h-12 rounded-xl flex justify-center items-center bg-green-700 hover:bg-green-500 text-white duration-300 ease-in-out"
+    >
+      Back
+    </p>
   </a>
 
-  <p class="w-full h-auto text-2xl md:text-4xl dark:text-white text-center my-12">
+  <p
+    class="w-full h-auto text-2xl md:text-4xl dark:text-white text-center my-12"
+  >
     Add User
   </p>
 </div>
 
-
 <div class="w-full h-auto flex justify-center items-center">
   <div
-    class="w-[90%] md:w-[50%] h-auto p-10 bg-[#94DCBA] dark:bg-[#11433A]  border border-[#11433A] dark:border-[#94DCBA] rounded-xl flex flex-col justify-center items-center gap-6"
+    class="w-[90%] md:w-[50%] h-auto p-10 bg-[#94DCBA] dark:bg-[#11433A] border border-[#11433A] dark:border-[#94DCBA] rounded-xl flex flex-col justify-center items-center gap-6"
   >
     <div class="w-full h-auto flex flex-col justify-center items-start">
       <p class="dark:text-white">Name</p>
@@ -48,8 +63,9 @@
       <p class="dark:text-white">Phone</p>
       <input
         type="text"
-        class="w-full bg-[#daffee] dark:bg-[#0d2621] rounded-xl border-0 dark:text-white"
+        class="phone-input w-full bg-[#daffee] dark:bg-[#0d2621] rounded-xl border-0 dark:text-white {userRequest.phone ? regexPhone.test(userRequest.phone) ? "outline-2 dark:outline-green-500":"outline-2 dark:outline-red-500" :"outline-none"}"
         bind:value={userRequest.phone}
+        on:input={phoneField}
       />
     </div>
 

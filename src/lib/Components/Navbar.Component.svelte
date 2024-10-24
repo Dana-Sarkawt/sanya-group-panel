98fe675b69d0f795cf808815f8015bfa7231f983
RovarQRoom <rovar.07000120@outlook.com>
2024-10-24 15:13:05 +0300

    Refactor Navbar and ProjectTable components, add Inbox link to Navbar

--- src/lib/Components/Navbar.Component.svelte
+++ src/lib/Components/Navbar.Component.svelte
@@ -126,6 +126,30 @@
             class:opacity-0={!activeUrl.startsWith("/project")}>Project</span
           >
         </NavLi>
+        <NavLi
+          href="/inbox"
+          class={`${activeUrl.startsWith("/inbox") ? activeClass : nonActiveClass} flex flex-col items-center justify-center transition-all duration-300 ease-in-out`}
+          on:click={toggleNavBar}
+        >
+          <svg
+            class="w-6 h-6"
+            fill="none"
+            stroke={getIconColor(activeUrl.startsWith("/inbox"))}
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
+            ></path>
+          </svg>
+          <span
+            class="mt-1 text-xs transition-opacity duration-300 ease-in-out"
+            class:opacity-0={!activeUrl.startsWith("/inbox")}>Inbox</span
+          >
+        </NavLi>
 
         <NavLi
           href="/user/0"

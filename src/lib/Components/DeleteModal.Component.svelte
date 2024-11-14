28d16bcf951dca0a0937e9399eecdfd00eb03fdf
RovarQRoom <rovar.07000120@outlook.com>
2024-11-14 15:50:30 +0300

    Refactor prettier configuration and postcss configuration

--- src/lib/Components/DeleteModal.Component.svelte
+++ src/lib/Components/DeleteModal.Component.svelte
@@ -1,48 +1,47 @@
 <script lang="ts">
-	import { toastStore } from '$lib/Store/Toast.Store';
-	import { Button, Modal } from 'flowbite-svelte';
-	import { _ } from 'svelte-i18n';
-	interface Props {
-		deleteModal?: boolean;
-		//@typescript-eslint/no-explicit-any
-		Store: any;
-		id?: number;
-	}
+  import { toastStore } from "$lib/Store/Toast.Store";
+  import { Button, Modal } from "flowbite-svelte";
+  import { _ } from "svelte-i18n";
+  interface Props {
+    deleteModal?: boolean;
+    Store: any;
+    id?: number;
+  }
 
-	let { deleteModal = $bindable(false), Store, id = 0 }: Props = $props();
+  let { deleteModal = $bindable(false), Store, id = 0 }: Props = $props();
 
-	async function deleteStore() {
-		try {
-			await Store.delete(id);
-			window.location.reload();
-		} catch (error) {
-			toastStore.error($_('failed-to-delete-item'));
-		} finally {
-			deleteModal = false;
-		}
-	}
+  async function deleteStore() {
+    try {
+      await Store.delete(id);
+      window.location.reload();
+    } catch (error) {
+      toastStore.error($_("failed-to-delete-item"));
+    } finally {
+      deleteModal = false;
+    }
+  }
 </script>
 
 <Modal
-	title="Delete Alert"
-	bind:open={deleteModal}
-	dialogClass="fixed top-0 start-0 end-0 h-modal md:inset-0 md:h-full z-[1000] w-full p-4 flex"
+  title="Delete Alert"
+  bind:open={deleteModal}
+  dialogClass="fixed top-0 start-0 end-0 h-modal md:inset-0 md:h-full z-[1000] w-full p-4 flex"
 >
-	<p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-		<span class="text-red-500">{$_('do-you-want-to-delete')}</span>
-	</p>
-	{@render footer()}
+  <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+    <span class="text-red-500">{$_("do-you-want-to-delete")}</span>
+  </p>
+  {@render footer()}
 </Modal>
 
 {#snippet footer()}
-	<Button
-		class="h-12 w-full rounded-xl bg-red-500 duration-300 ease-in-out hover:bg-red-400 dark:bg-red-500 dark:hover:bg-red-400"
-		on:click={() => {
-			deleteModal = false;
-		}}>{$_('no')}</Button
-	>
-	<Button
-		class="h-12 w-full rounded-xl bg-green-500 duration-300 ease-in-out hover:bg-green-400 dark:bg-green-500 dark:hover:bg-green-400"
-		on:click={deleteStore}>{$_('yes')}</Button
-	>
+  <Button
+    class="h-12 w-full rounded-xl bg-red-500 duration-300 ease-in-out hover:bg-red-400 dark:bg-red-500 dark:hover:bg-red-400"
+    on:click={() => {
+      deleteModal = false;
+    }}>{$_("no")}</Button
+  >
+  <Button
+    class="h-12 w-full rounded-xl bg-green-500 duration-300 ease-in-out hover:bg-green-400 dark:bg-green-500 dark:hover:bg-green-400"
+    on:click={deleteStore}>{$_("yes")}</Button
+  >
 {/snippet}
